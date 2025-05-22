9b39de51a9ab5b0fecdfd59669c858f51fc0566e
ElsayedIbrahim98 <128749398+ElsayedIbrahim98@users.noreply.github.com>
2025-05-22 13:49:21 +0200

    Update trainer.py

--- modules/trainer.py
+++ modules/trainer.py
@@ -76,7 +76,7 @@ class Trainer(nn.Module):
             on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/trainer_profile'),
             record_shapes=True,
             profile_memory=True,
-            # with_stack=True,
+            with_stack=True,
             # with_flops=True,
             # with_modules=True
             )
